defeabeaa1a3e0e67cad9f170c72d9b1bdaf2f50
y.ulyanets <y.ulyanets@plarium.com>
2017-11-22 13:28:06 +0200

    small rename

--- routes/classes.js
+++ routes/classes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://yuri:password@ds115546.mlab.com:15546/school_test_system', ['classes']);
 
-router.get('/tasks', function(req, res, next){
+router.get('/classes', function(req, res, next){
     db.classes.find(function(err, classes){
         if(err)
         {
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var tasks = require('./routes/tasks');
+var classes = require('./routes/classes');
 
 var port = 3000;
 var app = express();
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/', index);
-app.use('/api', tasks);
+app.use('/api', classes);
 
 app.listen(port, function(){
     console.log('Server started on port ' +port);
