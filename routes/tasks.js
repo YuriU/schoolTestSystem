55505f030ad7408bbc1815114652b60b70de1136
y.ulyanets <y.ulyanets@plarium.com>
2017-11-22 13:26:36 +0200

    connected to MongoDb

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://yuri:password@ds115546.mlab.com:15546/school_test_system', ['classes']);
 
 router.get('/tasks', function(req, res, next){
-    res.send('TASK API')
+    db.classes.find(function(err, classes){
+        if(err)
+        {
+            res.send(err);
+        }
+        else{
+            res.json(classes);
+        }  
+    });
 })
 
 module.exports = router;
